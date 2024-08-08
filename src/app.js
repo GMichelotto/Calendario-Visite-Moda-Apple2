1821293593d1b8bb2e19364b03be31228399345d
GMichelotto <50021075+GMichelotto@users.noreply.github.com>
2024-08-08 11:07:18 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    console.log('App component mounted');
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
