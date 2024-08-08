50a46a2d742d75fc600ea9671efc73b050eca88e
GMichelotto <50021075+GMichelotto@users.noreply.github.com>
2024-08-08 11:06:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+console.log('React app starting...');
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+console.log('React app rendered');
