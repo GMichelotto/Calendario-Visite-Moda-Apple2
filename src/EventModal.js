45caa2015ca3a72d4c53984eae253ef30a9afa1d
GMichelotto <50021075+GMichelotto@users.noreply.github.com>
2024-08-10 16:08:04 +0200

    Update EventModal.js

--- src/EventModal.js
+++ src/EventModal.js
@@ -1,60 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
-function EventModal({ event, onClose, onUpdate }) {
-  useEffect(() => {
-    import('./EventModal.css');
-  }, []);
-
-  const [editedEvent, setEditedEvent] = useState({
-    ...event,
-    start: moment(event.start).format('YYYY-MM-DDTHH:mm'),
-    end: moment(event.end).format('YYYY-MM-DDTHH:mm')
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setEditedEvent(prev => ({ ...prev, [name]: value }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onUpdate({
-      ...editedEvent,
-      start: new Date(editedEvent.start),
-      end: new Date(editedEvent.end)
-    });
-  };
-
-  return (
-    <div className="modal-backdrop">
-      <div className="modal">
-        <h2>Dettagli Evento</h2>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Cliente:
-            <input type="text" name="cliente" value={editedEvent.cliente} onChange={handleChange} />
-          </label>
-          <label>
-            Collezione:
-            <input type="text" name="collezione" value={editedEvent.collezione} onChange={handleChange} />
-          </label>
-          <label>
-            Inizio:
-            <input type="datetime-local" name="start" value={editedEvent.start} onChange={handleChange} />
-          </label>
-          <label>
-            Fine:
-            <input type="datetime-local" name="end" value={editedEvent.end} onChange={handleChange} />
-          </label>
-          <div className="button-group">
-            <button type="submit">Aggiorna</button>
-            <button type="button" onClick={onClose}>Chiudi</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+const styles = `
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-export default EventModal;
+.modal {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  width: 300px;
+}
+
+.modal h2 {
+  margin-top: 0;
+}
+
+.modal form {
+  display: flex;
+  flex-direction: column;
+}
+
+.modal label {
+  margin-bottom: 10px;
+}
+
+.modal input {
+  width: 100%;
+  padding: 5px;
+  margin-top: 5px;
+}
+
+.button-group {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+.button-group button {
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+.error-message {
+  color: red;
+  font-size: 0.8em;
+  margin-top: 5px;
+}
+`;
+
+function EventModal({ event
