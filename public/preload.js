24574e64b1d39dd7bcdb3f94765870918f61ac20
GMichelotto <50021075+GMichelotto@users.noreply.github.com>
2024-11-12 19:45:51 +0100

    Delete public/preload.js

--- public/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Esponi le funzionalità sicure al rendering process
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Aggiungi qui le funzioni che vuoi rendere disponibili al tuo codice React
-  // Esempio:
-  // sendMessage: (message) => ipcRenderer.send('send-message', message),
-  // onReceiveMessage: (callback) => ipcRenderer.on('receive-message', callback)
-});
