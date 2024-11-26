2f5e3f61c9b3158686dbdb80c6f673312185782a
GMichelotto <50021075+GMichelotto@users.noreply.github.com>
2024-11-26 19:10:16 +0100

    Create Index.tsx

---
+++ src/routes/Index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ClientiPage } from '../components/Clienti/ClientiPage';
+// Altri import...
+
+const AppRoutes: React.FC = () => {
+  return (
+    // Il tuo routing configuration qui
+  );
+};
+
+export default AppRoutes;
