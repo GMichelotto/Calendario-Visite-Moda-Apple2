4df0ce469d8b10682570f9c2d50b5ad464949309
GMichelotto <50021075+GMichelotto@users.noreply.github.com>
2024-12-19 18:53:23 +0100

    Update database.ts

--- src/types/database.ts
+++ src/types/database.ts
@@ -2,7 +2,7 @@
 export interface Cliente {
   id: number;
   ragione_sociale: string;
-  collezioni_ids?: string;
+  collezioni_ids: string;
   collezioni?: string[];
   collezioni_nomi?: string;
   indirizzo?: string;
