24a7409dd2a7c885aff9bbac028810fc277347f0
GMichelotto <50021075+GMichelotto@users.noreply.github.com>
2024-12-24 11:41:41 +0100

    Create types.ts

---
+++ shared/types.ts
@@ -0,0 +1,67 @@
+// shared/types.ts
+
+export interface ValidationResponse {
+  isValid: boolean;
+  errors: string[];
+  warnings: string[];
+  duration?: number;
+  checks: {
+    timeConstraints: boolean;
+    overlap: boolean;
+    clientAvailability: boolean;
+    collectionPeriod: boolean;
+    duration: boolean;
+  };
+}
+
+export interface EventValidationRequest {
+  cliente_id: string;
+  collezione_id: string;
+  data_inizio: string;
+  data_fine: string;
+  id?: number;
+}
+
+export interface CustomEvent {
+  id: number;
+  cliente_id: string;
+  collezione_id: string;
+  start: Date;
+  end: Date;
+  note?: string;
+}
+
+export interface ClienteResponse {
+  id: number;
+  ragione_sociale: string;
+  appointments_count?: number;
+  total_duration?: number;
+  [key: string]: any;
+}
+
+export interface CollezioneResponse {
+  id: number;
+  nome: string;
+  colore: string;
+  data_inizio: string;
+  data_fine: string;
+  [key: string]: any;
+}
+
+export interface EventoResponse {
+  id: number;
+  cliente_id: number;
+  collezione_id: number;
+  data_inizio: string;
+  data_fine: string;
+  note?: string;
+  [key: string]: any;
+}
+
+export interface EventFormData {
+  cliente_id: string;
+  collezione_id: string;
+  data_inizio: string;
+  data_fine: string;
+  note: string;
+}
