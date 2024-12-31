74447289444241a7d7321dc02734b2a99b3c4f07
GMichelotto <50021075+GMichelotto@users.noreply.github.com>
2024-12-31 11:05:54 +0100

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export {
+  ElectronAPI,
+  APIResponse,
+  Cliente,
+  Collezione,
+  Evento,
+  ValidationResponse,
+  ImportResult,
+  SlotAvailability,
+  ExportResult,
+  DashboardStats,
+  EventValidationRequest,
+  DatabaseOperations,
+  ClientiOperations,
+  CollezioniOperations,
+  EventiOperations
+} from '../../electron/types';
